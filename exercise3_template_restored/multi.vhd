3d5c23745f39ca45162eed1e246a36468f31255f
ThorSporring <90454987+ThorSporring@users.noreply.github.com>
2022-03-15 09:20:26 +0100

    Update multi.vhd

--- exercise3_template_restored/multi.vhd
+++ exercise3_template_restored/multi.vhd
@@ -3,22 +3,24 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity multi is 
-
+--Define your data width of the sum (Prod)
 generic -- mulig anvendelse af "generic"
 	(
-		DATA_WIDTH : natural := 15
+		N : natural := 15
 	);
     port
     (
         A : in std_logic_vector (7 downto 0),
         B : in std_logic_vector (7 downto 0),
-        Prod : out std_logic_vector (DATA_WIDTH downto 0)
+        Prod : out std_logic_vector (N downto 0)
     );
 end multi;
 
-architecture multi_impl of multi is 
 
+architecture multi_impl of multi is 
+--Declarations if necessary
 begin
+--Use logic operators for multiplication
     prod <= std_logic_vector(unsigned(a)*unsigned(b));
 
 end multi_impl;
