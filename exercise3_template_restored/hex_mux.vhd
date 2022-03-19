2b7024893f0dbe452be379749e40f8faf39be13e
ThorSporring <90454987+ThorSporring@users.noreply.github.com>
2022-03-19 16:07:02 +0100

    Fully done

--- exercise3_template_restored/hex_mux.vhd
+++ exercise3_template_restored/hex_mux.vhd
@@ -7,61 +7,34 @@ entity hex_mux is
     port(
         --input
         bin : in std_logic_vector (11 downto 0);
-		  sel : in std_logic_vector (1 downto 0);
+	    sel : in std_logic_vector (1 downto 0);
         --output
         tsseg: out std_logic_vector(20 downto 0)
     );
 end hex_mux;
 
 architecture hex_mux_impl of hex_mux is
---FÃ¸lgende instantierer 3 styks 7-segment displays. Hhv ss1, ss2, og ss3 
+ 
+--Midlertidigt signal for at gemme den potentielle besked der skal displayes
+    signal ssTemp: std_logic_vector(20 downto 0);
+--FÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¸lgende instantierer 3 styks 7-segment displays. Hhv ss1, ss2, og ss3
     begin
-	 
-	 --Lader input gÃ¥ til output (Standard mode)
-    if (sel = "10") then
-		ss1: entity work.bin_to_7_segment(bin_to_7_segment_impl)
-        port map
-        (
-            --input
-            bcd_input => bin(3 downto 0),
-            --output
-            s_seg => tsseg(6 downto 0)
-        );
-        ss2: entity work.bin_to_7_segment(bin_to_7_segment_impl)
-        port map
-        (
-            --input
-            bcd_input => bin(7 downto 4),
-            --output
-            s_seg => tsseg(13 downto 7)
-        );
-        ss3: entity work.bin_to_7_segment(bin_to_7_segment_impl)
-        port map
-        (
-            --input
-            bcd_input => bin(11 downto 8),
-            --output
-            s_seg => tsseg(20 downto 14)
-        );
-   
-	--Udskriver On message
-    else if (sel = "11") then
-        onMsg: entity work.bin_to_7_segment(bin_to_7_segment_impl)
-        port map
-        (
-        "0101011" => tsseg(6 downto 0),
-        "1000000" => tsseg(13 downto 7)
-        );
-    
-    --Udskriver error message 
-    else if (sel = "01") then 
-        errorMsg: entity work.bin_to_7_segment(bin_to_7_segment_impl)
-        port map
-        (
-        "0101111" => tsseg(6 downto 0),
-        "0101111" => tsseg(13 downto 7),
-        "0000110" => tsseg(20 downto 14)
-        );
-    end if;
+        ss1: entity work.bin_to_7_segment(bin_to_7_segment_impl)port map(
+            bcd_input => bin(3 downto 0), s_seg => ssTemp(6 downto 0));
+
+        ss2: entity work.bin_to_7_segment(bin_to_7_segment_impl)port map(
+            bcd_input => bin(7 downto 4), s_seg => ssTemp(13 downto 7));
+     
+        ss3: entity work.bin_to_7_segment(bin_to_7_segment_impl)port map(
+            bcd_input => bin(11 downto 8), s_seg => ssTemp(20 downto 14));
+
+            --On message, vises nÃ¥r ingen knapper trykkes pÃ¥
+        tsseg <= "1000000" & "0101011" & "1111111" when sel ="11" else
+            --Error message, vises nÃ¥r key 1 trykkes pÃ¥
+                 "0000110" & "0101111" & "0101111" when sel = "01" else
+            --NÃ¥r key 0 trykkes pÃ¥, viser det valgte tal fra input bin
+                 ssTemp when sel = "10" else 
+            --NÃ¥r begge alt andet sker, slukker displayet
+            "111111111111111111111"; 
 
     end hex_mux_impl;
