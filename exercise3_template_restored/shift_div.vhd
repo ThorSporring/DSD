75fabdfcd85468d25a32d576c967acb073f616ef
Tras94 <tobias-ras@live.dk>
2022-04-19 09:09:16 +0200

    hej

--- exercise3_template_restored/shift_div.vhd
+++ exercise3_template_restored/shift_div.vhd
@@ -11,19 +11,19 @@ entity shift_div is
 end shift_div;
 
 architecture shift_left_one of shift_div is
---bitshifter 8 bits Ã©n gang til venstre
+--bitshifter 8 bits en gang til venstre
     begin
         a_shl <= a(6 downto 0) & "0";
 end shift_left_one;
 
 architecture shift_right_two of shift_div is
---Bitshifter 8 bits 2 gange til hÃ¸jre
+--Bitshifter 8 bits 2 gange til højre
     begin
         a_shr <= "00" & a(7 downto 2);
 end shift_right_two;
 
 architecture rotate_three of shift_div is
---Roterer 8 bits 3 gange til hÃ¸jre
+--Roterer 8 bits 3 gange til højre
     begin
         a_ror <= a(2 downto 0 ) & a(7 downto 3);
  end rotate_three;
