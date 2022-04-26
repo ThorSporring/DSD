d6fbcc464b706a04627ec4892b9074b76bad3655
Tras94 <tobias-ras@live.dk>
2022-04-26 11:13:44 +0200

    s

--- Exercise_4_5/mux_last.vhd
+++ Exercise_4_5/mux_last.vhd
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity mux_last is -- Implementation of in and outputs
+entity mux_last is -- Implementation of in and outputs.
 	port(
 		player: in std_logic;
 		player1_hex: in std_logic_vector(13 downto 0);
@@ -11,14 +11,14 @@ entity mux_last is -- Implementation of in and outputs
 end mux_last;
 
 architecture mux_last_impl of mux_last is
-	-- Used to determine which input is going to the output
+	-- Used to determine which input is going to the output.
 	begin
 	mux_if2: process(player,player1_hex,player2_hex)
 		begin
 			if player = '1' then				
-				hex_out <= player2_hex;	-- if player is 1 player2_hex goes to the output
+				hex_out <= player2_hex;	-- if player is 1 player2_hex goes to the output.
 			else				
-				hex_out <= player1_hex;	-- if player is 0	player1_hex goes to the output
+				hex_out <= player1_hex;	-- if player is 0	player1_hex goes to the output.
 			end if;
 		end process mux_if2;
 end mux_last_impl;
