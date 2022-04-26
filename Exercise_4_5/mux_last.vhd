aff731610c19ed37ef797beaa74b1b4295a717a8
Tras94 <tobias-ras@live.dk>
2022-04-26 11:10:46 +0200

    f

--- Exercise_4_5/mux_last.vhd
+++ Exercise_4_5/mux_last.vhd
@@ -2,12 +2,12 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity mux_last is -- Implementation of in and outputs
-    port(
-		  player: in std_logic;
-		  player1_hex: in std_logic_vector(13 downto 0);
-		  player2_hex: in std_logic_vector(13 downto 0);
-		  hex_out: out std_logic_vector (13 downto 0)
-		  );
+	port(
+		player: in std_logic;
+		player1_hex: in std_logic_vector(13 downto 0);
+		player2_hex: in std_logic_vector(13 downto 0);
+		hex_out: out std_logic_vector (13 downto 0)
+		);
 end mux_last;
 
 architecture mux_last_impl of mux_last is
