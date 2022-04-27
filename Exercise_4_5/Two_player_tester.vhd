cad678b3e663ba23b530ef5ed9beac7499eac961
Tras94 <tobias-ras@live.dk>
2022-04-27 22:31:19 +0200

    er

--- Exercise_4_5/Two_player_tester.vhd
+++ Exercise_4_5/Two_player_tester.vhd
@@ -14,16 +14,16 @@ end Two_player_tester;
 architecture Two_player_tester_impl of Two_player_tester is
 	signal temp: std_logic_vector(8 downto 0);
     begin
-		-- Declares which local signals the modules inputs and outputs are connected to. 
-		--(Port Mapping)
+	-- Declares which local signals the modules inputs and outputs are connected to. 
+    --(Port Mapping)
         ggt: entity work.Two_player_guess_game(Two_player_guess_game_impl) port map(
-				input => SW(7 downto 0),
-				set => KEY(0),
-				try => KEY(1),
-				show => KEY(2),
-				hex1 => HEX0,
-				hex10 => HEX1,
-				player => SW(17),
-				hex_p => HEX7
+        input => SW(7 downto 0),
+         set => KEY(0),
+        try => KEY(1),
+        show => KEY(2),
+        hex1 => HEX0,
+         hex10 => HEX1,
+		player => SW(17),
+		hex_p => HEX7
         );
 end Two_player_tester_impl;
