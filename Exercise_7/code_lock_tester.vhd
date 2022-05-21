3939a76d1b53cda86dea5d843c7b98dbf3069d44
Tras94 <tobias-ras@live.dk>
2022-05-21 21:04:50 +0200

    d

--- Exercise_7/code_lock_tester.vhd
+++ Exercise_7/code_lock_tester.vhd
@@ -2,20 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity code_lock_tester is
-    port(
-        CLOCK_50: in std_logic;
-        SW: in std_logic_vector(3 downto 0);
-        KEY: in std_logic_vector(3 downto 0);
-        LEDG: out std_logic_vector(1 downto 0)
-    );
-end 
-
+	port(
+		SW: in std_logic_vector(3 downto 0);
+		KEY: in std_logic_vector(1 downto 0);
+		CLOCK_50: in std_logic;
+		LEDG: out std_logic_vector(2 downto 0)
+	);
+end code_lock_tester;
+	 
 architecture code_lock_tester_impl of code_lock_tester is
-    begin
-        clt: entity work.code_lock_simple(code_lock_simple_impl) port map(
-            enter => KEY(0),
-            reset => KEY(1),
-            code => SW(3 downto 0),
-            lock => LEDG(0)
-        );
+	begin
+		clt: entity work.code_lock(code_lock_impl) port map(
+			enter => KEY(0),
+			reset => KEY(1),
+			code => SW(3 downto 0),
+			lock => LEDG(0),
+			clk => CLOCK_50,
+			failed => LEDG(1)
+		);
 end code_lock_tester_impl;
